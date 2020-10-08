76da68ce2219be7cefaa1ec955e7e120f5284d59
senkuu <dorosario.enzo@protonmail.com>
2020-10-09 00:40:12 +0200

    add react-rewired and clean some code

--- src/components/Sponsor.js
+++ src/components/Sponsor.js
@@ -1,9 +1,6 @@
-import React from 'react';
-import './Sponsor.css'
+import React from "react";
+import "./Sponsor.css";
 
-export default function Sponsor(props){
-    return(
-        <img className="Image" src={props.info.backgroundImage}></img>
-        
-    )
+export default function Sponsor(props) {
+  return <img className="Image" src={props.info.backgroundImage}></img>;
 }
