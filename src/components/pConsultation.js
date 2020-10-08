283792d5ef3d1d45651dae9e0bc43771a84e8185
davy2407 <54894522+davy2407@users.noreply.github.com>
2020-10-09 00:10:43 +0200

    premiere consult
    je commence a travailler sur les function necessaire au composant pConsultation

--- src/components/pConsultation.js
+++ src/components/pConsultation.js
@@ -1,4 +1,6 @@
 import React, {useState, useEffect} from "react";
+import LightOn from "../images/lightOn.svg";
+import LightOff from "../images/lightOff.svg";
 import './pConsultation.css';
 
 
@@ -11,16 +13,88 @@ import './pConsultation.css';
 
 function PConsultation(props){
 
-    const [valueDecouverteGro, setDecouverteGro] = useState(
+        /// texte info IST 
+        const [ txtIST, setTxtIST] = useState(
+            "La HAS recommande un dépistage opportuniste ciblé à toutes les femmes enceintes consultant pour une IVG, sans limite d’âge."
+        )
+
+        const [currentInfoIST, setCurrentInfoIST] = useState(
+            ""
+        )
+
+        const affichageTxtIST = () => {
+            let txtAEnlever = currentInfoIST;
+            let txtAAfficher = txtIST;
+            setCurrentInfoIST(txtAAfficher);
+            setTxtIST(txtAEnlever);
+        }
+
+        const [logoAffiche, setLogoAffiche] = useState(
+            LightOff
+
+        )
+
+        const [logoNonAffiche, setLogoNonAffiche] = useState(
+            LightOn
+        )
+        const [ logoLampe,setLogoLampe]= useState(
+            {
+                on : LightOn,
+                off : LightOff
+            }
+        )
+
+        const changementCouleurSVG = () => {
+            let currentLampe = logoAffiche;
+            let currentCache = logoNonAffiche;
+            setLogoNonAffiche(currentLampe);
+            setLogoAffiche(currentCache);
+            affichageTxtIST();
+            
+
+        }
+
+        const [valueDecouverteGro, setDecouverteGro] = useState(
+        /// state bouton select ( ne fonctionne pas comme value de base actuellement )
         {value : "Selectionner"}
-    )
+        )
+        
+
+        /// Bloc consultation psychosocial 
+        const [consultationPsychoSo,setConsultationPsychoSo] = useState(
+            {
+                texte : "consultation psychosociale proposée",
+                className : "Red"
+            }
+        )
+
+        const [affichageWarningConsultation,setAffichageWarningConsultation] = useState(
+            {texte : "",
+            className: ""}
+
+        )
+
+        const afficheConsultation = () => {
+            setAffichageWarningConsultation(consultationPsychoSo)
+        }
 
 
-    const handleChange = (event) => {
+        const handleChange = (event) => {
+        /// gère le bouton select 
         console.log(event.target.value)
         let test = event.target.value
         setDecouverteGro({value: test});
-      }
+        }
+
+        ///BLoc DDR
+        const [recuptDateDDR,setRecuptDateDDR ] = useState(
+            {}
+
+        )
+        
+
+
+
    
 
   
@@ -44,7 +118,11 @@ Majeure Anonyme </h1>
                       assure la dispense
                      d’avance des frais permet de garantir un véritable anonymat de l’intervention ». Lien n°12</p>
                      <br></br>
-                     <form>
+                     <form
+                     onClick={(e)=>{
+                        e.preventDefault()
+        
+                      }}>
                          <h2>DDR</h2>
                         <label for="DDR">DDR « date des dernières règles » : </label>
                         <input
@@ -128,9 +206,192 @@ Ex : DDR : 1/08/2019, on est le 8/09/2019, ça fait donc 38 jours soit 5 semai
                                 <option value="Clinique">Clinique</option>
                             </select>
                         </label>
-                        {/* je reprends demain en fin d'aprem, je finis à 16h le taf  */}
+                        <h2>Contexte biopsychosocial favorable :</h2>
+                        {/* Bloc a finir  */}
+                        <br></br>
+                       <button>Oui</button>
+                        <br></br>
+                        
+                        <button onClick={afficheConsultation} >Non</button>
+                        <br></br>
+                        <p className={affichageWarningConsultation.className}>
+                           {affichageWarningConsultation.texte} 
+                        </p>
 
-                     </form>
+                        <h2>Recherche IST +/- antibio-prophylaxie</h2>
+                        <br></br>
+                        <label for="rchISTOui">  OUI  </label>
+                        <input
+                            type="radio"
+                            nom="rchISTOui"
+                            id="rchISTOui">
+                        </input>
+                        <br></br>
+                        <label for="rchISTNon">  NON  </label>
+                        <input
+                            type="radio"
+                            nom="rchISTNon"
+                            id="rchISTNon">
+                        </input>
+                        <br></br>
+                        <input onClick={changementCouleurSVG} className="Lampe" type="image" src={logoAffiche} />
+                        <p>  {currentInfoIST}
+                        </p>
+                        {/* Bloc a finir */}
+                        
+
+                        <br></br>
+                        <h2>Prise de sang (Groupe sanguin : 2 déterminations, RAI, -HCG quantitatif)</h2>
+                        <br></br>
+                        <p>? : « 2 typages de groupe sanguin sont nécessaires avant toute IVG médicamenteuse
+                             ou chirurgicale. Les RAI doivent dater de moins de 48h avant l’acte si négatif
+                              jusqu'alors.  A noter qu’une injection d’Immunoglobulines anti-D sera nécessaire
+                               en cas de RAI négatif avant toute IVG ou dans les 72h suivant tout saignement. »
+                        </p>
+
+                        <h2>Dossier guide IVG remis :</h2>
+                        <br></br>
+                        <label for="dossierIVGOui">  OUI  </label>
+                        <input
+                            type="radio"
+                            nom="dossierIVGOui"
+                            id="dossierIVGOui">
+                        </input>
+                        <br></br>
+                        <label for="dossierIVGNon">  NON  </label>
+                        <input
+                            type="radio"
+                            nom="dossierIVGNon"
+                            id="dossierIVGNon">
+                        </input>
+                        <br></br>
+
+
+                        <p>Lien dossier guide IVG PDF</p>
+                        {/* lien à créer */}
+
+
+                        <h2>Consultation psychosociale proposée</h2>
+                        <br></br>
+                        <label for="consultationOui">  OUI  </label>
+                        <input
+                            type="radio"
+                            nom="consultationOui"
+                            id="consultationOui">
+                        </input>
+                        <br></br>
+                        <label for="consultationNon">  NON  </label>
+                        <input
+                            type="radio"
+                            nom="consultationNon"
+                            id="consultationNon">
+                        </input>
+                        <br></br>
+                        <p>ontexte biopsychosocial favorable : si réponse « non » précédemment 
+                             faire apparaître « consultation psychosociale proposée » en rouge.
+                             {/* Bloc a finir */}
+                        </p>
+                        <br></br>
+                        <p>? : « Elle n’est pas obligatoire pour les personnes majeures. 
+                            Il existe l’obligation légale de le proposer.  La patiente a un délai de
+                             48h de réflexion avant de signer le consentement après cette consultation.
+                             {/* bulle information a faire  */}
+                        </p>
+                        <br></br>
+                        <p>Elle peut avoir lieu : </p>
+                        <br></br>
+                        <ul>
+                            <li>dans un établissement d’information, de consultation ou de conseil familial </li>
+                            <li>dans un centre de planification ou d’éducation familiale</li>
+                            <li>dans un service social ou un autre organisme agréé</li>
+                        </ul>
+                        <br></br>
+                        <h2>Information contraception post-IVG</h2>
+                        <br></br>
+                        <label for="infoContraOui">  OUI  </label>
+                        <input
+                            type="radio"
+                            nom="infoContraOui"
+                            id="infoContraOui">
+                        </input>
+                        <br></br>
+                        <label for="infoContraNon">  NON  </label>
+                        <input
+                            type="radio"
+                            nom="infoContraNon"
+                            id="infoContraNon">
+                        </input>
+                        <br></br>
+                        <p>? : « Il est nécessaire d’aborder dès cette consultation,
+                             les différentes modalités contraceptives. Site possible
+                              d’information à transmettre à la patiente : 
+                              Lien <a href="https://www.choisirsacontraception.fr/?gclid=EAIaIQobChMI8_jf5raj7AIVD9d3Ch2qZwTZEAAYASAAEgIlxvD_BwE">choisirsacontraception.com</a></p>
+                        
+                        
+
+                     
+                     <br></br>
+                     <h2>Frottis à jour </h2>
+                     <br></br>
+                        <label for="frottiOui">  OUI  </label>
+                        <input
+                            type="radio"
+                            nom="frottiOui"
+                            id="frottiOui">
+                        </input>
+                        <br></br>
+                        <label for="frottiNon">  NON  </label>
+                        <input
+                            type="radio"
+                            nom="frottiNon"
+                            id="frottiNon">
+                        </input>
+                        <br></br>
+                        <label for="nonConcerNon">  NON CONCERNEE  </label>
+                        <input
+                            type="radio"
+                            nom="nonConcerNon"
+                            id="nonConcerNon">
+                        </input>
+                        <br></br>
+                        <label for="dateFrotti">Date : </label>
+                        <input
+                            type="date"
+                            name="dateFrotti"
+                            id="dateFrotti">
+
+                        </input>
+                        <br></br>
+                        <p>? « Le plan de dépistage national s’applique,
+                             pour toute femme immunocompétente de 25 à 65 ans.
+                        </p>
+                        <a href="https://www.ameli.fr/assure/sante/themes/cancer-col-uterus/frottis-depistage">https://www.ameli.fr/assure/sante/themes/cancer-col-uterus/frottis-depistage
+                        </a>
+                        <br></br>
+                        <h2>Tabac :</h2>
+                        <br></br>
+                        <label for="tabacOui">  OUI  </label>
+                        <input
+                            type="radio"
+                            nom="tabacOui"
+                            id="tabacOui">
+                        </input>
+                        <br></br>
+                        <label for="tabacNon">  NON  </label>
+                        <input
+                            type="radio"
+                            nom="tabacNon"
+                            id="tabacNon">
+                        </input>
+                        <br></br>
+                        <label for="nbPAquet">  Nombre paquets   </label>
+                        <input
+                            type="text"
+                            nom="nbPAquet"
+                            id="nbPAquet">
+                        </input>
+                        </form>
+                        
             
           </div>
     )
