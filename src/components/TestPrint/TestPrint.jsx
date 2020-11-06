072880f87c761fe3e993a8f8517f7bac0bb4cca8
davy2407 <54894522+davy2407@users.noreply.github.com>
2020-11-06 03:53:17 +0100

    remise a plat test print

--- src/components/TestPrint/TestPrint.jsx
+++ src/components/TestPrint/TestPrint.jsx
@@ -13,15 +13,13 @@ class TestPrint extends React.Component {
   render() {
     
     return (
-        <div className="containerTestPrint" >
-       
-          <img src={Logo} className="logoT"></img>
+        <div className="containerR" >
+                  <img src={Logo} className="logo"></img>
 
 
-       
       <br></br>
       <br></br>
-      <h3 className="centerEnd">
+      <h3>
         Résumé de la consultation de 1er contact médical préalable à l’IVG :{" "}
       </h3>
       <br></br>
@@ -35,7 +33,7 @@ class TestPrint extends React.Component {
       <br></br>
       <br></br>
       
-    <div className="DroiteEnd">{this.state.data.map((objet)=>{
+    <div >{this.state.data.map((objet)=>{
     return (
       <div>
         
@@ -51,15 +49,15 @@ class TestPrint extends React.Component {
       <br></br>
     <br></br>
       <br></br>
-      <p className="centerEnd">Il est à noter qu’un délai de 48h entre la consultation psychosociale et la 2eme consultation préalable à l’IVG est à respecter (non obligatoire). </p>
+      <p>Il est à noter qu’un délai de 48h entre la consultation psychosociale et la 2eme consultation préalable à l’IVG est à respecter (non obligatoire). </p>
       <br></br>
       <br></br>
-      <p className="dateEnd">Date et Tampon</p>
+      <p>Date et Tampon</p>
       <br></br>
       <br></br>
       <br></br>
       <br></br>
-      <p className="centerEnd">Ces données ont été recueillies de façon anonyme et gratuite à partir du site IVGclic.com.</p>
+      <p>Ces données ont été recueillies de façon anonyme et gratuite à partir du site IVGclic.com.</p>
 
       </div>
     );
