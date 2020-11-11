085a9deeff5d1f512573663788bda1d2dde58adb
davy2407 <54894522+davy2407@users.noreply.github.com>
2020-11-11 21:58:07 +0100

    txt premiere prise maj min

--- src/components/PremierePriseMineureConsultePrin/PremierePriseMineureConsultePrin.js
+++ src/components/PremierePriseMineureConsultePrin/PremierePriseMineureConsultePrin.js
@@ -1189,32 +1189,7 @@ Cette violence peut être physique, sexuelle, économique, verbale ou psychologi
   <div>{currentOui}</div>
 
   
-  <div className="BulleInfo">
-              <p>
-              Médicaments nécessaires à l’acte vendus en pharmacie d’officine. Exclusivement aux médecins ou aux sages-femmes ayant passé convention avec un établissement de santé. 
-              </p>
-              <p>
-              Commande à usage professionnel auprès de la pharmacie d’officine de son choix. La commande comprend les mentions suivantes :
-              </p>
-              
-              <ul>
-                  <li>
-                  Le nom, la qualité, le numéro d’inscription à l’Ordre, l’adresse et la 
-signature du praticien ainsi que la date
-                  </li>
-                  <li>
-                  Le nom des médicaments et le nombre de boîtes commandées
-                  </li>
-                  <li>
-                  La mention « usage professionnel
-                  </li>
-                  <li>
-                  Le nom de l’établissement de santé avec lequel le praticien a conclu une convention ainsi que la date de cette convention.
-                  </li>
-
-
-              </ul>
-          </div>
+ 
 
       <br></br>
           </div>
