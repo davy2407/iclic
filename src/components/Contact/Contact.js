d25a17650f1ddbbde1c2eaaf660d1c58d2178441
davy2407 <54894522+davy2407@users.noreply.github.com>
2023-01-18 02:10:47 +0100

    nom

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -13,7 +13,10 @@ function Contact() {
             <br></br>
             <h2>Responsable éditorial : </h2>
             <p>
-            PUECH Lilian, médecin généraliste.
+            PUECH Lilian : médecin généraliste.
+            </p>
+            <p>
+                Mr WEISS Julien : interne en médecine générale.
             </p>
             <p>
             Pour toutes demande, n’hésitez pas à nous contacter via contact.ivgclic@gmail.com
@@ -21,12 +24,13 @@ function Contact() {
 
             <h2>Intégration, développement et graphisme : </h2>
             <p>
+            Mr Bindé Davy : Chargé de mission principal du projet.
+            </p>
+            <p>
             Mr Degre Thibaud : Graphiste, en charge de l’élaboration du logo.
 
             </p>
-            <p>
-            Mr Bindé Davy : Chargé de mission principal du projet.
-            </p>
+            
             <p>
             Mr Do rozario Enzo : Chargé de l’aide à la conception.
             </p>
